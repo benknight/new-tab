439fe78f6fefd2905369661028c26d6d2bda681c
Benjamin Knight <ben@benknight.me>
2014-12-04 11:10:23 -0800

    Adding a holiday theme

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 (function () {
-    var emoticons = [
-        '♘', '★', '☯', '♥', '✈', '✓', '☃', '❄︎', '⌘', '✪', '☂', '➳'
-    ];
-    emoticons.sort(function() { return 0.5 - Math.random(); });
-    document.querySelector('#x').innerHTML = emoticons.join('').substr(0, 7);
+  var emoticons = [
+    // Default set
+    // '♘', '★', '☯', '♥', '✈', '✓', '☃', '❄︎', '⌘', '✪', '☂', '➳'
+
+    // Christmas
+    '❅', '❆', '❄', '✴', '✶', '✵', '❈', '✷'
+  ];
+  emoticons.sort(function() { return 0.5 - Math.random(); });
+  document.querySelector('#x').innerHTML = emoticons.join('').substr(0, 7);
 })();
