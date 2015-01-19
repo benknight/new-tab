056ac43c33b846307e2b37ff333869319f6aff65
Benjamin Knight <ben@benknight.me>
2015-01-19 02:17:38 -0800

    Adding jquery, renaming things

--- emoticons.js
+++ emoticons.js
@@ -1,10 +1,10 @@
 (function () {
   var emoticons = [
     // Default set
-    // '♘', '★', '☯', '♥', '✈', '✓', '☃', '❄︎', '⌘', '✪', '☂', '➳'
+    '♘', '★', '☯', '♥', '✈', '✓', '☃', '❄︎', '⌘', '✪', '☂', '➳'
 
     // Christmas
-    '❅', '❆', '❄', '✴', '✶', '✵', '❈', '✷'
+    // '❅', '❆', '❄', '✴', '✶', '✵', '❈', '✷'
   ];
   emoticons.sort(function() { return 0.5 - Math.random(); });
   document.querySelector('#x').innerHTML = emoticons.join('').substr(0, 7);
