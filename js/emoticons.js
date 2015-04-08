9965a60f19e567a336f9339bc141785f561efa84
Benjamin Knight <ben@benknight.me>
2015-04-08 09:30:20 -0700

    Removing everything but the reddit QOD

--- js/emoticons.js
+++
@@ -1,9 +0,0 @@
-var emoticons = [
-	// Default set
-	'♘', '★', '☯', '♥', '✈', '✓', '☃', '❄︎', '⌘', '✪', '☂', '➳'
-
-	// Winter
-	// '❅', '❆', '❄', '✴', '✶', '✵', '❈', '✷'
-];
-emoticons.sort(function() { return 0.5 - Math.random(); });
-document.querySelector('#emoticons').innerHTML = emoticons.join('').substr(0, 7);
